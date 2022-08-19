0d7295f0a79e7dbe9e44a00fedfa63745d750276
aanderson60 <alexd43anderson@gmail.com>
2022-08-18 22:40:56 -0500

    TI updates

--- src/scripts/class.js
+++ src/scripts/class.js
@@ -1,5 +1,5 @@
 var currDate = new Date().getTime();
-var seniorDate = new Date("August 22, 2023 07:00:00 CST").getTime();
+var seniorDate = new Date("August 18, 2023 07:00:00 CST").getTime();
 var intro1 = "I am a"
 var intro2 = "electrical engineer planning to graduate in May 2023, or in:"
 function checkDate() {
